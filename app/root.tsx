9a2ccb368a1de120696aebc0758508980b9f725e
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-30 00:01:38 +0300

    chore: final root fixes and server sync

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { json, type LinksFunction, type LoaderFunctionArgs } from "@remix-run/node";
 import {
     Links,
     Meta,
