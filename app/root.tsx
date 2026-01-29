dd170e86f6f842c79445e22e51c3eab5dfc77296
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-29 23:57:03 +0300

    fix: add shopify-api-key meta and app-bridge script

--- app/root.tsx
+++ app/root.tsx
@@ -15,15 +15,22 @@ export const links: LinksFunction = () => [
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return await addDocumentResponseHeaders(request, {});
+  await addDocumentResponseHeaders(request, {});
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY,
+  });
 };
 
 export default function App() {
+  const { apiKey } = useLoaderData<typeof loader>();
+
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="shopify-api-key" content={apiKey} />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
