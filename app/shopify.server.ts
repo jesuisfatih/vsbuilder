da4294979cf44729a93a9590382b0c6b6978ce34
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-30 00:02:16 +0300

    feat: upgrade to shopify api 2026-04

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -1,8 +1,7 @@
 import "@shopify/shopify-app-remix/adapters/node";
 import {
-  ApiVersion,
-  AppDistribution,
-  shopifyApp,
+    AppDistribution,
+    shopifyApp
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
@@ -10,7 +9,7 @@ import prisma from "./db.server";
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.January25,
+  apiVersion: "2026-04" as any,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
@@ -26,7 +25,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = ApiVersion.January25;
+export const apiVersion = "2026-04";
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
