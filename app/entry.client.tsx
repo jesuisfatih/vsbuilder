a6763321382c8f68f7a9ecf999d5990017f0ffdc
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-30 23:46:51 +0300

    fix: Normalize App Proxy URL before hydration for React Router matching

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,32 @@
 /**
- * By default, Remix will handle hydrating your app on the client for you.
- * For more information, see https://remix.run/file-conventions/entry.client
+ * Entry Client - URL Normalization for App Proxy
+ * Browser URL: /apps/vsbuilder/editor or /en-us/apps/vsbuilder/editor
+ * Internal Route: /proxy/editor
+ *
+ * We normalize the URL before React hydration so Router can match correctly.
  */
 
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+
+// Normalize App Proxy URLs to internal route format BEFORE hydration
+function normalizeAppProxyUrl() {
+  const pathname = window.location.pathname;
+
+  // Pattern: /apps/vsbuilder/... or /XX-XX/apps/vsbuilder/...
+  // Convert to: /proxy/...
+  const appProxyPattern = /^(\/[a-z]{2}-[a-z]{2})?\/apps\/vsbuilder\//;
+
+  if (appProxyPattern.test(pathname)) {
+    const newPath = pathname.replace(appProxyPattern, '/proxy/');
+    console.log('[entry.client] Normalizing URL:', pathname, '->', newPath);
+    window.history.replaceState(null, '', newPath + window.location.search);
+  }
+}
+
+// Normalize URL before hydration
+normalizeAppProxyUrl();
 
 startTransition(() => {
   hydrateRoot(
