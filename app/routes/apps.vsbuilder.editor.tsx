2b733ffb558ac5f0763684a49767eac945a44bb3
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-31 22:48:10 +0300

    fix: Missing imports for theme sync

--- app/routes/apps.vsbuilder.editor.tsx
+++ app/routes/apps.vsbuilder.editor.tsx
@@ -9,7 +9,7 @@ import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { ClientOnlyEditor } from "../components/ClientOnlyEditor";
 import { authenticate } from "../shopify.server";
-import { downloadThemeForEditor } from "../utils/theme.server";
+import { downloadThemeForEditor, isThemeSavedLocally, saveThemeToLocal } from "../utils/theme.server";
 import { EditorCore } from "./app.editor";
 
 
